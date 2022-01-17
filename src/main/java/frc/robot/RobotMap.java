6203a5688cca2a5eb65c518674bc49214507e1a2
Sidd Rao <siddrao.926@gmail.com>
2022-01-17 07:26:19 -0800

    Added DriveBase To RobotMap

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+public final class RobotMap {
+    public final class DriveBase {
+
+        /**
+         * TODO: Set CAN IDs Based On Production Robot
+         */
+        
+        public static final int FRONT_LEFT_MODULE_DRIVE = 0;
+        public static final int FRONT_LEFT_MODULE_STEERING = 1;
+        public static final int FRONT_LEFT_MODULE_ENCODER = 2;
+
+        public static final int FRONT_RIGHT_MODULE_DRIVE = 3;
+        public static final int FRONT_RIGHT_MODULE_STEERING = 4;
+        public static final int FRONT_RIGHT_MODULE_ENCODER = 5;
+
+        public static final int BACK_LEFT_MODULE_DRIVE = 6;
+        public static final int BACK_LEFT_MODULE_STEERING = 7;
+        public static final int BACK_LEFT_MODULE_ENCODER = 8;
+
+        public static final int BACK_RIGHT_MODULE_DRIVE = 9;
+        public static final int BACK_RIGHT_MODULE_STEERING = 10;
+        public static final int BACK_RIGHT_MODULE_ENCODER = 11;
+
+        public static final int PIGEON_IMU = 12;
+    }
+}
