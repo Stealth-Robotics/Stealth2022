c4319f51375cf15507cef187617b5debb6f70096
Sidd Rao <siddrao.926@gmail.com>
2022-01-17 09:03:42 -0800

    Get And Set Swerve Module States

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -6,19 +6,25 @@ import com.swervedrivespecialties.swervelib.SwerveModule;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 
-public class DriveBase {
+public class DriveBase extends SubsystemBase {
+
     private final PigeonIMU pigeon = new PigeonIMU(RobotMap.DriveBase.PIGEON_IMU);
 
     private final SwerveModule frontLeftModule;
     private final SwerveModule frontRightModule;
     private final SwerveModule backLeftModule;
     private final SwerveModule backRightModule;
+
+    private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
 
     public DriveBase() {
         ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
@@ -65,7 +71,7 @@ public class DriveBase {
                 RobotMap.DriveBase.BACK_RIGHT_MODULE_ENCODER,
                 Constants.DriveBase.BACK_RIGHT_MODULE_STEER_OFFSET);
     }
-    
+
     public Rotation2d getGyroscopeRotation() {
         return Rotation2d.fromDegrees(pigeon.getFusedHeading());
     }
@@ -76,5 +82,39 @@ public class DriveBase {
 
     public void zeroGyroscope() {
         setGyroscopeRotation(0);
+    }
+
+    public void drive(double vxMetersPerSecond, double vyMetersPerSecond, double omegaRadiansPerSecond) {
+        this.chassisSpeeds = new ChassisSpeeds(
+                vxMetersPerSecond,
+                vyMetersPerSecond,
+                omegaRadiansPerSecond);
+    }
+
+    public void drive(ChassisSpeeds chassisSpeeds) {
+        this.chassisSpeeds = chassisSpeeds;
+    }
+
+    @Override
+    public void periodic() {
+        SwerveModuleState[] states = Constants.DriveBase.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND);
+
+        frontLeftModule.set(
+                states[0].speedMetersPerSecond / Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND
+                        * Constants.DriveBase.MAX_VOLTAGE,
+                states[0].angle.getRadians());
+        frontRightModule.set(
+                states[1].speedMetersPerSecond / Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND
+                        * Constants.DriveBase.MAX_VOLTAGE,
+                states[1].angle.getRadians());
+        backLeftModule.set(
+                states[2].speedMetersPerSecond / Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND
+                        * Constants.DriveBase.MAX_VOLTAGE,
+                states[2].angle.getRadians());
+        backRightModule.set(
+                states[3].speedMetersPerSecond / Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND
+                        * Constants.DriveBase.MAX_VOLTAGE,
+                states[3].angle.getRadians());
     }
 }
