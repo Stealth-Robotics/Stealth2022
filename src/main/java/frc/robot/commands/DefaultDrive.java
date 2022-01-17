adc5e5d682afc2e55a7bc91563f49e336338ffa7
Sidd Rao <siddrao.926@gmail.com>
2022-01-17 09:10:05 -0800

    Default Drive Command

---
+++ src/main/java/frc/robot/commands/DefaultDrive.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveBase;
+
+import java.util.function.DoubleSupplier;
+
+public class DefaultDrive extends CommandBase {
+    private final DriveBase driveBase;
+
+    private final DoubleSupplier translationXSupplier;
+    private final DoubleSupplier translationYSupplier;
+    private final DoubleSupplier rotationSupplier;
+
+    public DefaultDrive(DriveBase driveBase,
+                               DoubleSupplier translationXSupplier,
+                               DoubleSupplier translationYSupplier,
+                               DoubleSupplier rotationSupplier) {
+        this.driveBase = driveBase;
+        this.translationXSupplier = translationXSupplier;
+        this.translationYSupplier = translationYSupplier;
+        this.rotationSupplier = rotationSupplier;
+
+        addRequirements(driveBase);
+    }
+
+    @Override
+    public void execute() {
+        driveBase.drive(
+                ChassisSpeeds.fromFieldRelativeSpeeds(
+                        translationXSupplier.getAsDouble(),
+                        translationYSupplier.getAsDouble(),
+                        rotationSupplier.getAsDouble(),
+                        driveBase.getGyroscopeRotation()
+                )
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        driveBase.drive(0.0, 0.0, 0.0);
+    }
+}
