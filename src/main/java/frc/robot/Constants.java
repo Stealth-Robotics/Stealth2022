89e51d213e15b074577ed92a09faff8db8d606ea
Sidd Rao <siddrao.926@gmail.com>
2022-01-17 08:42:53 -0800

    Added Swerve Drive Kinematics Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,21 +1,10 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
- */
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 public final class Constants {
     public static final class DriveBase {
         /**
@@ -31,9 +20,29 @@ public final class Constants {
          * (In Degrees)
          */
 
-        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 0.0;
-        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 0.0;
-        public static final double BACK_LEFT_MODULE_STEER_OFFSET = 0.0;
-        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 0.0;
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+
+        public static final double MAX_VOLTAGE = 12.0;
+
+        // TODO: Fix Module Configuration Based On Production Robot
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
+                SdsModuleConfigurations.MK4_L4.getDriveReduction() *
+                SdsModuleConfigurations.MK4_L4.getWheelDiameter() * Math.PI;
+
+        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+                Math.hypot(TRACKWIDTH / 2.0, WHEELBASE / 2.0);
+
+        public final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
+                // Front left
+                new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                // Front right
+                new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
+                // Back left
+                new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                // Back right
+                new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0));
     }
 }
