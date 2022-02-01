9678ce9557eda4dfbb13e7aa8d25bd329a2545b8
Samarth Rao <raosamarth91@gmail.com>
2022-01-31 17:56:38 -0800

    Added Lime System

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,56 +4,66 @@ import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 
 public final class Constants {
-        public static final class Shooter
-       {
-        public static final double ShooterP = 0;
-        public static final double ShooterI = 0;
-        public static final double ShooterD = 0;
+        public static final class Limelight {
+                public static final double targetHeight = 2.49; // meters
+                public static final double crosshairHeight = 2.27; // meters | old: 2.23, old: 2.3876 | height of center
+                public static final double cameraHeight = 0.65405; // in meters
+                public static final double cameraAngle = 27 * (Math.PI / 180); // in radians
+                                             
 
-        public static final double HoodP = 0;
-        public static final double HoodI = 0;
-        public static final double HoodD = 0;
-       }
-    public static final class DriveBase {
-        /**
-         * TODO: Fix TRACKWIDTH & WHEELBASE Based On Production Robot
-         * (In Meters)
-         */
+        }
 
-        public static final double TRACKWIDTH = 0.0; // Distance From Left Wheel Middle To Right Wheel Middle
-        public static final double WHEELBASE = 0.0; // Distance From Front Wheel Middle To Back Wheel Middle
+        public static final class Shooter {
+                public static final double ShooterP = 0;
+                public static final double ShooterI = 0;
+                public static final double ShooterD = 0;
 
-        /**
-         * TODO: Fix Module Steer Offsets Based On Production Robot
-         * (In Degrees)
-         */
+                public static final double HoodP = 0;
+                public static final double HoodI = 0;
+                public static final double HoodD = 0;
+        }
 
-        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+        public static final class DriveBase {
+                /**
+                 * TODO: Fix TRACKWIDTH & WHEELBASE Based On Production Robot
+                 * (In Meters)
+                 */
 
-        public static final double MAX_VOLTAGE = 12.0;
+                public static final double TRACKWIDTH = 0.0; // Distance From Left Wheel Middle To Right Wheel Middle
+                public static final double WHEELBASE = 0.0; // Distance From Front Wheel Middle To Back Wheel Middle
 
-        public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
-                SdsModuleConfigurations.MK4_L2.getDriveReduction() *
-                SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
+                /**
+                 * TODO: Fix Module Steer Offsets Based On Production Robot
+                 * (In Degrees)
+                 */
 
-        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
-                Math.hypot(TRACKWIDTH / 2.0, WHEELBASE / 2.0);
+                public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+                public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+                public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+                public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
 
-        public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
-                // Front left
-                new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-                // Front right
-                new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
-                // Back left
-                new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-                // Back right
-                new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0));
-    }
-    
+                public static final double MAX_VOLTAGE = 12.0;
+
+                public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
+                                SdsModuleConfigurations.MK4_L2.getDriveReduction() *
+                                SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
+
+                public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+                                Math.hypot(TRACKWIDTH / 2.0, WHEELBASE / 2.0);
+
+                public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
+                                // Front left
+                                new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                                // Front right
+                                new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
+                                // Back left
+                                new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                                // Back right
+                                new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0));
+        }
+
 }
---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,103 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Limelight extends SubsystemBase 
+{
+    NetworkTable limelightTableEntry;
+
+    public Limelight()
+    {
+        limelightTableEntry = NetworkTableInstance.getDefault().getTable("limelight");
+
+        intializeLimelight();
+    }
+
+    @Override
+    public void periodic()
+    {
+        //System.out.println(getTargetDistance());
+    }
+
+    /**
+     * Intializes Limelight LED Mode & Camera Mode
+     */
+    public void intializeLimelight()
+    {
+        setLedMode(3);
+        setCamMode(0);
+    }
+
+    /**
+     * Checks if Limelight has a valid target
+     * 
+     * @return if the Limelight has a valid target
+     */
+    public boolean hasValidTarget()
+    {
+        return limelightTableEntry.getEntry("tv").getDouble(0) == 1;
+    }
+
+    /**
+     * Returns Horizontal Offset Of The Target From The Crosshair
+     * 
+     * @return If the limelight has a valid target, it will return the horizontal offset of the target from the crosshair. If the limelight doesn't have a valid target, it will return NaN.
+     */
+    public double getTargetHorizontalOffset()
+    {
+        return hasValidTarget() ? limelightTableEntry.getEntry("tx").getDouble(0) : Double.NaN;
+    }
+
+    /**
+     * The angle between the camera and the target
+     * 
+     * @return The angle, in radians
+     */
+    public double getTargetVerticalOffset()
+    {
+        return hasValidTarget() ? limelightTableEntry.getEntry("ty").getDouble(0) * Math.PI / 180 : Double.NaN;
+    }
+
+    /**
+     * Target Area (0% of image to 100% of image)
+     * 
+     * @return Target Size 0% of image to 100% of image, will return NaN if no valid target
+     */
+    public double getTargetArea()
+    {
+        return hasValidTarget() ? limelightTableEntry.getEntry("ta").getDouble(0) : Double.NaN;
+    }
+
+    /**
+     * Gets the distance from the shooter to the power port
+     * 
+     * @return The distance, in meters
+     */
+    public double getTargetDistance()
+    {
+        return (Constants.Limelight.crosshairHeight - Constants.Limelight.cameraHeight) / Math.tan(Constants.Limelight.cameraAngle + getTargetVerticalOffset());
+    }
+
+    public void getCamMode(double defaultValue)
+    {
+        limelightTableEntry.getEntry("camMode").getDouble(defaultValue);
+    }
+
+    public void getLedMode(double defaultValue)
+    {
+        limelightTableEntry.getEntry("ledMode").getDouble(defaultValue);
+    }
+
+    public void setCamMode(double camMode)
+    {
+        limelightTableEntry.getEntry("camMode").setNumber(camMode);
+    }
+
+    public void setLedMode(double ledMode)
+    {
+        limelightTableEntry.getEntry("ledMode").setNumber(ledMode);
+    }
+}
