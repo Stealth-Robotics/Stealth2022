f88dbf023998aa18cc855e8f0d3bcc6bce7b3419
Samarth Rao <raosamarth91@gmail.com>
2022-02-19 15:06:26 -0800

    Changed CAN ID's to match with robot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,23 +9,23 @@ public final class RobotMap {
          * TODO: Set CAN IDs Based On Production Robot
          */
 
-        public static final int FRONT_LEFT_MODULE_DRIVE = 0;
-        public static final int FRONT_LEFT_MODULE_STEERING = 1;
-        public static final int FRONT_LEFT_MODULE_ENCODER = 2;
+        public static final int FRONT_LEFT_MODULE_DRIVE = 13;
+        public static final int FRONT_LEFT_MODULE_STEERING = 23;
+        public static final int FRONT_LEFT_MODULE_ENCODER = 33;
 
-        public static final int FRONT_RIGHT_MODULE_DRIVE = 3;
-        public static final int FRONT_RIGHT_MODULE_STEERING = 4;
-        public static final int FRONT_RIGHT_MODULE_ENCODER = 5;
+        public static final int FRONT_RIGHT_MODULE_DRIVE = 14;
+        public static final int FRONT_RIGHT_MODULE_STEERING = 24;
+        public static final int FRONT_RIGHT_MODULE_ENCODER = 34;
 
-        public static final int BACK_LEFT_MODULE_DRIVE = 6;
-        public static final int BACK_LEFT_MODULE_STEERING = 7;
-        public static final int BACK_LEFT_MODULE_ENCODER = 8;
+        public static final int BACK_LEFT_MODULE_DRIVE = 11;
+        public static final int BACK_LEFT_MODULE_STEERING = 21;
+        public static final int BACK_LEFT_MODULE_ENCODER = 31;
 
-        public static final int BACK_RIGHT_MODULE_DRIVE = 9;
-        public static final int BACK_RIGHT_MODULE_STEERING = 10;
-        public static final int BACK_RIGHT_MODULE_ENCODER = 11;
+        public static final int BACK_RIGHT_MODULE_DRIVE = 12;
+        public static final int BACK_RIGHT_MODULE_STEERING = 22;
+        public static final int BACK_RIGHT_MODULE_ENCODER = 32;
 
-        public static final int PIGEON_IMU = 12;
+        public static final int PIGEON_IMU = 45;
 
     }
 
@@ -44,10 +44,10 @@ public final class RobotMap {
          * TODO: Set CAN IDs Based On Production Robot
          */
 
-        public static final int SHOOTER_MOTOR_1 = 2;
-        public static final int SHOOTER_MOTOR_2 = 2;
-        public static final int HOOD_MOTOR = 3;
-        public static final int HOOD_SWITCH = 8;
+        public static final int SHOOTER_MOTOR_1 = 8;
+        public static final int SHOOTER_MOTOR_2 = 9;
+        public static final int HOOD_MOTOR = 7;
+        public static final int HOOD_SWITCH = 0;
     }
 
     public static final class Conveyor {
--- src/main/java/frc/robot/commands/ShooterCommands/AimHood.java
+++ src/main/java/frc/robot/commands/ShooterCommands/AimHood.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.ShooterCommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AimHood extends CommandBase{
     
