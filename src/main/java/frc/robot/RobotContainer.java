a65d91087271ca66618270136e5e8b568794e337
Samarth Rao <raosamarth91@gmail.com>
2022-02-21 23:18:40 -0800

    Made Shooter Stop and Hood Reset

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -22,7 +23,6 @@ import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Limelight;
-
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -87,9 +87,14 @@ public class RobotContainer {
         new SequentialCommandGroup(
             // new InstantCommand(() -> limelight.setLedMode(3)),
             // add align to targer and ready shooter in a parralel deadline
-            new ReadyShooter(shooter, 5 /*limelight.getTargetDistance()*/),
+            new ReadyShooter(shooter, 5 /* limelight.getTargetDistance() */),
             new MoveConveyor(conveyor, Constants.Conveyor.SHOOT_CONVEYOR_STEP * 2),
-            new RunCommand(() -> shooter.hoodToPos(0))));
+            new ParallelCommandGroup(
+                new RunCommand(() -> shooter.hoodToPos(0)),
+                new RunCommand(() -> shooter.setVelocity(0))
+            )
+        )
+    );
   }
 
   /**
