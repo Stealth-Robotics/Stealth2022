c8d22dbe0f9270ef451c3ac748d1f75f7a2aa398
Sidd Rao <siddrao.926@gmail.com>
2022-03-02 11:32:13 -0800

    Removed SwerveControllerCommand Path Following

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,6 @@ import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
@@ -37,12 +35,8 @@ public final class Constants {
                                 SdsModuleConfigurations.MK4_L2.getDriveReduction() *
                                 SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
 
-                public static final double MAX_ACCELERATION_METERS_PER_SECOND = Units.feetToMeters(10);
-
                 public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
                                 Math.hypot(TRACKWIDTH / 2.0, WHEELBASE / 2.0);
-
-                public static final double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 3 * Math.PI;
 
                 public static final double SLOWMODE_MULTIPLIER = 1 / 4;
 
@@ -55,30 +49,6 @@ public final class Constants {
                                 new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
                                 // Back right
                                 new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0));
-
-                public static final double X_P_CONTROLLER = 0.0;
-                public static final double X_I_CONTROLLER = 0.0;
-                public static final double X_D_CONTROLLER = 0.0;
-                public static final double X_TOLERANCE = 0.0;
-
-                public static final double Y_P_CONTROLLER = 0.0;
-                public static final double Y_I_CONTROLLER = 0.0;
-                public static final double Y_D_CONTROLLER = 0.0;
-                public static final double Y_TOLERANCE = 0.0;
-
-                public static final double THETA_P_CONTROLLER = 0.0;
-                public static final double THETA_I_CONTROLLER = 0.0;
-                public static final double THETA_D_CONTROLLER = 0.0;
-                public static final double THETA_TOLERANCE = 0.0;
-
-                public static final TrapezoidProfile.Constraints THETA_CONTROLLER_CONSTRAINTS = new TrapezoidProfile.Constraints(
-                                MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
-                                MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED);
-
-                public static final TrajectoryConfig CONFIG = new TrajectoryConfig(
-                                Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND,
-                                Constants.DriveBase.MAX_ACCELERATION_METERS_PER_SECOND)
-                                                .setKinematics(Constants.DriveBase.DRIVE_KINEMATICS);
 
                 public static final double ALIGN_P_COEFF = 0;
                 public static final double ALIGN_I_COEFF = 0;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,31 +4,16 @@
 
 package frc.robot;
 
-import java.util.List;
-
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ConveyerCommands.ConveyorDefault;
 import frc.robot.commands.DriveBaseCommands.DriveDefault;
-import frc.robot.commands.DriveBaseCommands.FollowTrajectory;
 import frc.robot.commands.IntakeCommands.IntakeDefault;
 import frc.robot.commands.MultiSubsystemCommands.ShootCargo;
 import frc.robot.subsystems.Climber;
@@ -59,7 +44,7 @@ public class RobotContainer {
 
         private final UsbCamera intakeCamera;
 
-       private final Joystick buttonPanel = new Joystick(Constants.IO.MECH_JOYSTICK_PORT);
+        private final Joystick buttonPanel = new Joystick(Constants.IO.MECH_JOYSTICK_PORT);
         private final XboxController driveGamepad = new XboxController(Constants.IO.DRIVE_JOYSTICK_PORT);
 
         /**
@@ -77,7 +62,7 @@ public class RobotContainer {
                 intake.setDefaultCommand(new IntakeDefault(intake,
                                 driveGamepad::getRightTriggerAxis, driveGamepad::getLeftTriggerAxis));
 
-                conveyor.setDefaultCommand(new ConveyorDefault(conveyor, () ->  driveGamepad.getStartButton() ));
+                conveyor.setDefaultCommand(new ConveyorDefault(conveyor, () -> driveGamepad.getStartButton()));
 
                 intakeCamera = CameraServer.startAutomaticCapture(0);
                 intakeCamera.setResolution(1280, 720);
@@ -106,17 +91,8 @@ public class RobotContainer {
                 new JoystickButton(driveGamepad, 4).whenHeld(new InstantCommand(() -> climber.setSpeed(-0.3)))
                                 .whenReleased(() -> climber.setSpeed(0));
 
-                new JoystickButton(driveGamepad, 2).whenPressed(new InstantCommand(() ->
-                climber.togglePivotPistons()));
-
+                new JoystickButton(driveGamepad, 2).whenPressed(new InstantCommand(() -> climber.togglePivotPistons()));
         }
-
-        SequentialCommandGroup testAutoSPath = new SequentialCommandGroup(
-                        new FollowTrajectory(driveBase, TrajectoryGenerator.generateTrajectory(
-                                        new Pose2d(0, 0, new Rotation2d(0)),
-                                        List.of(),
-                                        new Pose2d(3, 0, new Rotation2d(0)),
-                                        Constants.DriveBase.CONFIG)));
 
         /**
          * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -124,46 +100,6 @@ public class RobotContainer {
          * @return the command to run in autonomous
          */
         public Command getAutonomousCommand() {
-
-                // TrajectoryConfig config = new TrajectoryConfig(
-                //                 Constants.DriveBase.MAX_VELOCITY_METERS_PER_SECOND,
-                //                 Constants.DriveBase.MAX_ACCELERATION_METERS_PER_SECOND)
-                //                                 // Add kinematics to ensure max speed is actually obeyed
-                //                                 .setKinematics(Constants.DriveBase.DRIVE_KINEMATICS);
-
-                // Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-                //                 // Start at the origin facing the +X direction
-                //                 new Pose2d(0, 0, new Rotation2d(0)),
-                //                 // Pass through these two interior waypoints, making an 's' curve path
-                //                 List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                //                 // End 3 meters straight ahead of where we started, facing forward
-                //                 new Pose2d(3, 0, new Rotation2d(0)),
-                //                 config);
-
-                // var thetaController = new ProfiledPIDController(
-                //                 1, 0, 0,
-                //                 new TrapezoidProfile.Constraints(
-                //                                 Constants.DriveBase.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
-                //                                 Constants.DriveBase.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
-                // thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-                // SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-                //                 exampleTrajectory,
-                //                 driveBase::getPose, // Functional interface to feed supplier
-                //                 Constants.DriveBase.DRIVE_KINEMATICS,
-
-                //                 // Position controllers
-                //                 new PIDController(1, 0, 0),
-                //                 new PIDController(1, 0, 0),
-                //                 thetaController,
-                //                 driveBase::setModuleStates,
-                //                 driveBase);
-
-                // driveBase.resetOdometry(exampleTrajectory.getInitialPose());
-
-                // return swerveControllerCommand.andThen(() -> driveBase.drive(0, 0, 0));
-
-                // An ExampleCommand will run in autonomous
-                return testAutoSPath;
+                return new InstantCommand();
         }
 }
