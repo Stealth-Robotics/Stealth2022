053af5ef3076e476980657ebf16e7d09a761ed1a
Samarth Rao <raosamarth91@gmail.com>
2022-03-01 18:32:38 -0800

    Made function to eject top cargo

---
+++ src/main/java/frc/robot/commands/MultiSubsystemCommands/EjectTopCargo.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.MultiSubsystemCommands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.ConveyerCommands.MoveConveyor;
+import frc.robot.commands.ShooterCommands.ResetShooter;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Shooter;
+
+public class EjectTopCargo extends SequentialCommandGroup {
+    private final Conveyor conveyor;
+    private final Shooter shooter;
+
+    public EjectTopCargo(Shooter shooter, Conveyor conveyor) {
+        this.conveyor = conveyor;
+        this.shooter = shooter;
+
+        addCommands(
+                new ParallelCommandGroup(
+                        new InstantCommand(() -> shooter.setVelocity(1000)),
+                        new InstantCommand(() -> shooter.hoodToDegree(70))),
+                new MoveConveyor(conveyor, Constants.Conveyor.SHOOT_CONVEYOR_STEP),
+                new ResetShooter(shooter));
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.setVelocity(0);
+        shooter.hoodToPos(0);
+    }
+}
