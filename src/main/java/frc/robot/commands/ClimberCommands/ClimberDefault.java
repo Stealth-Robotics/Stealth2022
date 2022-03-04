87c848b3c33a7313e89e20d37f18ef89870486fe
Sidd Rao <siddrao.926@gmail.com>
2022-03-03 23:24:27 -0800

    Changed Auto Climber

--- src/main/java/frc/robot/commands/ClimberCommands/ClimberDefault.java
+++ src/main/java/frc/robot/commands/ClimberCommands/ClimberDefault.java
@@ -12,12 +12,17 @@ public class ClimberDefault extends CommandBase {
     private final DoubleSupplier upSpeed;
     private final DoubleSupplier downSpeed;
     private final BooleanSupplier override;
+    private final BooleanSupplier overrideUp;
+    private final BooleanSupplier toggleButton;
 
-    public ClimberDefault(Climber climber, DoubleSupplier upSpeed, DoubleSupplier downSpeed, BooleanSupplier override) {
+    public ClimberDefault(Climber climber, DoubleSupplier upSpeed, DoubleSupplier downSpeed, BooleanSupplier override,
+            BooleanSupplier overrideUp, BooleanSupplier toggleButton) {
         this.climber = climber;
         this.upSpeed = upSpeed;
         this.downSpeed = downSpeed;
         this.override = override;
+        this.overrideUp = overrideUp;
+        this.toggleButton = toggleButton;
 
         addRequirements(climber);
     }
@@ -31,22 +36,21 @@ public class ClimberDefault extends CommandBase {
     public void execute() {
 
         if (downSpeed.getAsDouble() > 0.05
-                && climber.getClimberPosition() > Constants.ClimberConstants.MIN_THRESHOLD) {
+                && climber.getClimberPosition() > Constants.ClimberConstants.MIN_THRESHOLD
+                || override.getAsBoolean()) {
             climber.setSpeed(-downSpeed.getAsDouble() * Constants.ClimberConstants.MAX_SPEED);
         }
 
-        else if (climber.getClimberPosition() < Constants.ClimberConstants.MAX_THRESHOLD
-                && upSpeed.getAsDouble() > 0.05) {
-            climber.setSpeed(upSpeed.getAsDouble() * Constants.ClimberConstants.MAX_SPEED);
+        else if (overrideUp.getAsBoolean()
+                && climber.getClimberPosition() > Constants.ClimberConstants.MIN_THRESHOLD
+                || override.getAsBoolean()) {
+            climber.setSpeed(-Constants.ClimberConstants.MAX_SPEED);
         }
 
-        // else if (downSpeed.getAsDouble() > 0.05 && override.getAsBoolean()) {
-        //     climber.setSpeed(-downSpeed.getAsDouble() * Constants.ClimberConstants.MAX_SPEED);
-        // }
-
-        // else if (upSpeed.getAsDouble() > 0.05 && override.getAsBoolean()) {
-        //     climber.setSpeed(upSpeed.getAsDouble() * Constants.ClimberConstants.MAX_SPEED);
-        // }
+        else if (climber.getClimberPosition() < Constants.ClimberConstants.MAX_THRESHOLD
+                && upSpeed.getAsDouble() > 0.05 || override.getAsBoolean()) {
+            climber.setSpeed(upSpeed.getAsDouble() * Constants.ClimberConstants.MAX_SPEED);
+        }
 
         else {
             climber.setSpeed(0);
@@ -59,6 +63,8 @@ public class ClimberDefault extends CommandBase {
                 && climber.getClimberPosition() > Constants.ClimberConstants.MAX_THRESHOLD / 4
                 && !override.getAsBoolean()) {
             climber.movePisitons(true);
+        } else if (override.getAsBoolean() && toggleButton.getAsBoolean()) {
+            climber.togglePivotPistons();
         }
 
     }
