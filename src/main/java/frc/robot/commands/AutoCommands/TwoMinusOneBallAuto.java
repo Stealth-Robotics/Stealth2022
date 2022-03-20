f3cc0f4eb830b23a36905735508accc10841ed35
yimale <61242020+yimale@users.noreply.github.com>
2022-03-20 01:31:53 -0700

    Auto prep

---
+++ src/main/java/frc/robot/commands/AutoCommands/TwoMinusOneBallAuto.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.AutoCommands;
+
+import java.util.List;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.commands.DriveBaseCommands.SwerveControllerFollower;
+import frc.robot.commands.MultiSubsystemCommands.ShootCargo;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
+
+public class FiveBallAuto extends SequentialCommandGroup {
+    public FiveBallAuto(DriveBase driveBase, Intake intake, Shooter shooter, Conveyor conveyor, Limelight limelight) {
+        addRequirements(driveBase, intake, shooter, conveyor, limelight);
+
+        addCommands(
+                new InstantCommand(() -> driveBase.resetOdometry(new Pose2d())),
+                new InstantCommand(() -> intake.deploy()),
+                new InstantCommand(() -> intake.setSpeed(1)),
+                //follow 5BallPath1
+                new ShootCargo(driveBase, shooter, conveyor, limelight));
+    }
+}
