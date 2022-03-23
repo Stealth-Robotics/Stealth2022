d096d532a489defbbd2ccf1be0b1f132384e5fa2
yimale <61242020+yimale@users.noreply.github.com>
2022-03-22 23:35:57 -0700

    more auto climb changes

--- src/main/java/frc/robot/commands/ClimberCommands/AutoClimb.java
+++ src/main/java/frc/robot/commands/ClimberCommands/AutoClimb.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.ClimberCommands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Climber;
@@ -11,41 +12,32 @@ public class AutoClimb extends SequentialCommandGroup{
     public AutoClimb(Climber c) {
         this.climb = c;
         addRequirements(climb);
-    }
 
-
-    @Override
-    public void initialize() {
-
-        //climb to mid
-        climb.movePisitons(false);
-        climb.climberToPos(0);
-        //TODO wait until finished
+        addCommands(
+                         //climb to mid
+        new InstantCommand(() -> climb.movePisitons(false)),
+        new MoveClimber(climb, 0),
 
         //climb to high
-        climb.movePisitons(true);
-        climb.climberToPos(20000);
-        //TODO wait until finished
-        climb.climberToPos(25000);
-        climb.movePisitons(false);
-        new WaitCommand(.5); 
-        climb.climberToPos(0);
-        //TODO wait until finished
+        new InstantCommand(() -> climb.movePisitons(true)),
+        new MoveClimber(climb, 20000),
+        new WaitCommand(.5),
+        new MoveClimber(climb, 25000),
+        new InstantCommand(() -> climb.movePisitons(false)),
+        new WaitCommand(.5), 
+        new MoveClimber(climb, 0),
         
         //climb to traverse
-        climb.movePisitons(true);
-        climb.climberToPos(20000);
-        new WaitCommand(.5);
-        climb.climberToPos(25000);
-        //TODO wait until finished
-        climb.movePisitons(false);
-        new WaitCommand(.5); 
-        climb.climberToPos(10000);
+        new InstantCommand(() -> climb.movePisitons(true)),
+        new MoveClimber(climb, 20000),
+        new WaitCommand(.5),
+        new MoveClimber(climb, 25000),
 
-    }
+        new InstantCommand(() -> climb.movePisitons(false)),
+        new WaitCommand(.5),
+        new MoveClimber(climb, 10000)
+        );
 
-    public boolean isFinished() {
-        return false; //TODO add end condition
     }
 
 }
--- src/main/java/frc/robot/commands/ClimberCommands/MoveClimber.java
+++ src/main/java/frc/robot/commands/ClimberCommands/MoveClimber.java
@@ -1,25 +1,26 @@
 package frc.robot.commands.ClimberCommands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class RaiseClimber extends SequentialCommandGroup {
+public class MoveClimber extends CommandBase{
     private final Climber climb;
+    private final int newPos;
 
 
-    public RaiseClimber(Climber c) {
+    public MoveClimber(Climber c, int newpos) {
         this.climb = c;
+        this.newPos = newpos;
         addRequirements(climb);
     }
 
     @Override
     public void initialize() {
-        climb.climberToPos(20000);
+        climb.climberToPos(newPos);
     }
 
+    @Override
     public boolean isFinished() {
         return climb.climberAtPos();
     }
-
 }
