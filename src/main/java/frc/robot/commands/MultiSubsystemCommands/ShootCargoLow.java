c8e4b90bae41eae9027771cb540f899763513e9a
Sidd Rao <siddrao.926@gmail.com>
2022-03-26 22:00:15 -0700

    Added Wait Until Up To Speed To Low Goal

--- src/main/java/frc/robot/commands/MultiSubsystemCommands/ShootCargoLow.java
+++ src/main/java/frc/robot/commands/MultiSubsystemCommands/ShootCargoLow.java
@@ -3,6 +3,7 @@ package frc.robot.commands.MultiSubsystemCommands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
 import frc.robot.commands.ConveyerCommands.MoveConveyor;
 import frc.robot.commands.DriveBaseCommands.AlignWithTarget;
@@ -33,6 +34,7 @@ public class ShootCargoLow extends SequentialCommandGroup {
                 new MoveConveyor(conveyor, -500),
                 new InstantCommand(() -> shooter.setSpeed(.3)),
                 new InstantCommand(() -> shooter.hoodToDegree(72)),
+                new WaitUntilCommand(() -> shooter.hoodAtPos()),
                 new MoveConveyor(conveyor, Constants.ConveyorConstants.SHOOT_CONVEYOR_STEP * 2),
                 new ResetShooter(shooter));
     }
