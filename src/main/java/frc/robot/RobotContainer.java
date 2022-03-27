1cdc3f1a827e39d2f3a9ef40166979612ebee756
Sidd Rao <siddrao.926@gmail.com>
2022-03-26 22:03:23 -0700

    Cleaned Up Robot Container
    Removed Cluttering Comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -34,17 +33,8 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
+
     private final DriveBase driveBase = new DriveBase();
     private final Intake intake = new Intake();
     private final Shooter shooter = new Shooter();
@@ -61,9 +51,6 @@ public class RobotContainer {
     SendableChooser<Command> autoChooser = new SendableChooser<>();
     UsbCamera camera = CameraServer.startAutomaticCapture();
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
     public RobotContainer() {
         autoChooser.setDefaultOption("Five Ball Auto",
                 new FiveBallAuto(driveBase, intake, shooter, conveyor, limelight));
@@ -96,18 +83,9 @@ public class RobotContainer {
         climber.setDefaultCommand(new ClimberDefault(climber, () -> mechGamepad.getRawAxis(3),
                 () -> mechGamepad.getRawAxis(4), () -> mechGamepad.getRawButton(6)));
 
-        // Configure the button bindings
         configureButtonBindings();
     }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
     private void configureButtonBindings() {
         // drive gamepad controls
         new JoystickButton(driveGamepad, 1)
@@ -128,11 +106,6 @@ public class RobotContainer {
 
     }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
     public Command getAutonomousCommand() {
 
         return autoChooser.getSelected()
